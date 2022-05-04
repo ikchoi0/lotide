98e23d989828a414e5ccf5085978c61c0a2ae746
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-04 15:52:03 -0400

    counts occurances of unique letters in input string

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let result = {};
+  for (let letter of sentence) {
+    if (letter === ' ') {
+      continue;
+    }
+    if (result[letter]) {
+      result[letter]++;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
