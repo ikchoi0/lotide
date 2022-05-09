ee00bec52f1750b285f5fe2bf9e0380e770b10ef
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-09 14:30:20 -0400

    refactored assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(1, 1);
-assertEqual(1, 2);
+
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
+
 /*
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
