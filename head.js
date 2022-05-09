993e44660bce3bab6e9c77e14e9e44c5ed406a9b
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-09 14:37:59 -0400

    refactored tail.js

--- head.js
+++ head.js
@@ -1,13 +1,3 @@
-const assertEqual = require('./assertEqual');
-
-
-/*
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Lighthouse", "Lighthouse");
-assertEqual(1, 1);
-assertEqual(1, 2);
-*/
 const head = function(arr) {
   if (arr === []) {
     return undefined;
@@ -15,5 +5,4 @@ const head = function(arr) {
     return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   if (arr.length === 0 || arr.length === 1) {
     return [];
@@ -17,23 +8,4 @@ const tail = function(arr) {
   }
 };
 
-
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.join(' '), ["Lighthouse", "Labs"].join(' '));
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-let test = [];
-assertEqual(tail(test).join(' '), [].join(' '));
-test = [1,2,3];
-assertEqual(tail(test).join(' '), [2, 3].join(' '));
-test = [];
-assertEqual(tail(test).join(' '), [1].join(' '));
-test = [1];
-assertEqual(tail(test).join(' '), [1].join(' '));
-  
-
-
-
-
+module.exports = tail;
